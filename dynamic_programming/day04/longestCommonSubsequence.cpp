530149b4dc4f78d635abf7443cbd18160dafa903
zed <1037270467@qq.com>
2023-11-28 23:00:59 +0800

    day04

---
+++ dynamic_programming/day04/longestCommonSubsequence.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+// #include<unordered_set>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        int m = text1.size();
+        int n = text2.size();
+        vector<vector<int>> dp(m+1, vector<int>(n+1));
+        for (int i=1;i<m+1;i++){
+            char c1 =text1[i-1];
+            for(int j=1;j<n+1;j++){
+                char c2 = text2[j-1];
+                if(c1==c2){
+                    dp[i][j] = dp[i-1][j-1]+1;
+                }
+                else{
+                    dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+};
